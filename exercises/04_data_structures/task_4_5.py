7361b6ddbe278e0f2e7b9b0e4ab9b69a0239fc82
Mikhail-Zakharov <z-mike@mail.ru>
2020-11-03 13:53:24 +0300

    Done task_4_5

--- exercises/04_data_structures/task_4_5.py
+++ exercises/04_data_structures/task_4_5.py
@@ -13,3 +13,11 @@
 
 command1 = "switchport trunk allowed vlan 1,2,3,5,8"
 command2 = "switchport trunk allowed vlan 1,3,8,9"
+vlans1 = command1.split()[-1] # Разделяем строку на блоки (разделитель пробел) и берем последний блок
+vlans2 = command2.split()[-1]
+vlans1 = vlans1.split(',') # Разделяем на отдельные символы (разделитель запятая)
+vlans2 = vlans2.split(',') 
+vlans = set(vlans1) & set(vlans2) # Преобразуем в множества и делаем пресечение множеств
+vlans = list(vlans)  # Преобразуем в список
+vlans = sorted(vlans)
+print(vlans) 
