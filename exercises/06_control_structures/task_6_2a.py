ee3f022c3258d5fa4e3db61f80df2efadeba27e2
Mikhail-Zakharov <z-mike@mail.ru>
2020-11-06 18:51:10 +0300

    Done task_6_2a

--- exercises/06_control_structures/task_6_2a.py
+++ exercises/06_control_structures/task_6_2a.py
@@ -17,3 +17,25 @@
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 
 """
+
+ip = input('Ввведите IP адрес в формате 1.2.3.4: ')
+try:
+    octet1 = int(ip.split('.')[0])
+    octet2 = int(ip.split('.')[1])
+    octet3 = int(ip.split('.')[2])
+    octet4 = int(ip.split('.')[3])
+except ValueError:
+    print ('Неправильный IP-адрес')
+else:
+    if ip == '255.255.255.255':
+        print('local broadcast')
+    elif ip == '0.0.0.0':
+        print('unassigned')
+    elif octet1 > 255 or octet2 > 255 or octet3 > 255 or octet4 > 255:
+        print ('Не правильный IP-адрес')
+    elif octet1 >= 1 and octet1 <=223:
+        print('unicast')    
+    elif octet1 >= 224 and octet1 <=239:
+        print('multicast')
+    else:
+        print('unused')
