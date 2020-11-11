6a76f0f9750cbc68c3208efa79553f3b3dfaa8f8
Mikhail-Zakharov <z-mike@mail.ru>
2020-11-11 10:57:58 +0300

    Done task 7_2a 7_2b

--- exercises/07_files/task_7_2a.py
+++ exercises/07_files/task_7_2a.py
@@ -12,4 +12,14 @@
 
 """
 
-ignore = ["duplex", "alias", "Current configuration"]
+ignore = [" duplex", "alias", "Current configuration"]
+
+from sys import argv
+
+file_name = argv[1]
+
+f = open(file_name)
+
+for line in f:
+        if line.find('!') and line.find(ignore[0]) and line.find(ignore[1]) and line.find(ignore[2]) is -1:
+            print(line.strip())
--- exercises/07_files/task_7_2b.py
+++ exercises/07_files/task_7_2b.py
@@ -14,3 +14,17 @@
 """
 
 ignore = ["duplex", "alias", "Current configuration"]
+
+from sys import argv
+
+file_name = argv[1]
+
+f = open(file_name)
+f2 = open('config_sw1_cleared.txt', 'w')     
+
+for line in f:
+        if line.find(ignore[0]) and line.find(ignore[1]) and line.find(ignore[2]) is -1:
+            #print(line.strip())
+            f2.writelines(line)
+f.close()
+f2.close()  
